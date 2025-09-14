8d4eb07a28d177ef6045a2814e62adc22f4a45ee
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-14 14:21:09 +0530

    ye

--- employees/models.py
+++ employees/models.py
@@ -7,3 +7,7 @@ class Employee(models.Model):
   photo = models.ImageField(upload_to='images')
   designation= models.CharField(max_length=100)
   email_address= models.EmailField(max_length=100,unique=True)
+  phone_number = models.CharField(max_length = 12, blank = True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
