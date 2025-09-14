006fca28e22532f3ff56245d5672d2dab8a68afb
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-14 14:18:41 +0530

    ye

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+  first_name = models.CharField(max_length =100)
+  last_name = models.CharField(max_length=100)
+  photo = models.ImageField(upload_to='images')
+  designation= models.CharField(max_length=100)
+  email_address= models.EmailField(max_length=100,unique=True)
