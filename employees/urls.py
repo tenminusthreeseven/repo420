285c8b9646e920199847e3fb4f0b85c4c8f55285
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-17 18:06:14 +0530

    ye

--- employees/urls.py
+++ employees/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('<int:pk>/', views.employee_details),
+    path('', views.home, name='employee_home'),
+    path('<int:pk>/', views.employee_detail, name='employee_detail'),  # Correct function name
 ]
